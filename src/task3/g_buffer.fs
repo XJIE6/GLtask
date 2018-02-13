d0a66aebbc90d098e10bf5fccd2b7e2be13062f0
XJIE6 <krav4enkoyura@yandex.ru>
2018-02-13 23:17:41 +0300

    task3 v1

---
+++ src/task3/g_buffer.fs
@@ -0,0 +1,18 @@
+#version 330 core
+layout (location = 0) out vec3 gPosition;
+layout (location = 1) out vec3 gNormal;
+
+in vec2 TexCoords;
+in vec3 FragPos;
+in vec3 Normal;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
+void main()
+{    
+    // store the fragment position vector in the first gbuffer texture
+    gPosition = FragPos;
+    // also store the per-fragment normals into the gbuffer
+    gNormal = -normalize(Normal);
+}
